5b050fcfe3610cc71caa432de96fb6e9e2da8339
Diego Armando <00188625@uca.edu.sv>
2025-11-30 17:42:37 -0600

    Add comments for data structures and search methods
    Add comments explaining data structures and search methods.

---
+++ apuntesfinales.cpp
@@ -0,0 +1,7 @@
+//guardar datos unicos y ordenados set
+//relacionar clave -> valor y buscar por clave map  asocia datos (relacion)
+//busqueda por clave
+//claves ordenadas
+//si no necesito orden. pero quiero relacion clave - > valor, tendre que usar unordered_map
+//binary search solo funciona si el vector esta ordenado, por eso el vector se ordena antes con un sort 
+struct player{
